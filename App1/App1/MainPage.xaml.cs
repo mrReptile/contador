9d0c748ba708bcf1914f8328d4411b72deb674c3
Israel Vidales Licea <2018371096@uteq.edu.mx>
2021-09-16 16:21:25 -0500

    cambio

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+           InitializeComponent();
+        }
+
+        private void aumento(object sender, EventArgs e)
+        {
+            int conteo = int.Parse(contador.Text);
+            
+            conteo++;
+
+            string conteo2 = conteo.ToString();
+
+            contador.Text = conteo2;
+        }
+    }
+}
